ed3ff6be190769c951ad7f8afe54676ec3bdfa6a
Robert Meacher <robert@filteragency.com>
2025-05-27 11:41:04 +0100

    Add PHP code linting

--- includes/batchImageAltText.php
+++ includes/batchImageAltText.php
@@ -1,4 +1,7 @@
 <?php
+/**
+ * Batch image alt text functions
+ */
 
 use Felix_Arntz\AI_Services\Services\API\Enums\AI_Capability;
 use Felix_Arntz\AI_Services\Services\API\Enums\Content_Role;
@@ -6,346 +9,403 @@ use Felix_Arntz\AI_Services\Services\API\Types\Content;
 use Felix_Arntz\AI_Services\Services\API\Types\Parts;
 use Felix_Arntz\AI_Services\Services\API\Helpers;
 
+/**
+ * Get list of supported image mime types
+ *
+ * @return string[] Array of supported image mime types
+ */
 function filter_ai_mime_types() {
-  $post_mime_types = array(
-    // disable avif as Open AI doesn't current suport it
-    // https://platform.openai.com/docs/guides/images-vision?api-mode=responses#image-input-requirements
-    //'image/avif',
-    'image/gif',
-    'image/jpeg',
-    'image/png',
-    'image/webp',
-  );
+	$post_mime_types = array(
+		// disable avif as Open AI doesn't currently suport it
+		// https://platform.openai.com/docs/guides/images-vision?api-mode=responses#image-input-requirements
+		// 'image/avif',
+		'image/gif',
+		'image/jpeg',
+		'image/png',
+		'image/webp',
+	);
 
-  return implode(',', $post_mime_types);
+	return implode( ',', $post_mime_types );
 }
 
-function filter_ai_get_unique_attachments($attachments) {
-  $uniqueAttachments = [];
+/**
+ * Filter array of attachments and returns unique items based on $attachment->guid
+ *
+ * @param mixed[] $attachments Array of attachments
+ *
+ * @return mixed[] Retuns an array of unique attachments
+ */
+function filter_ai_get_unique_attachments( $attachments ) {
+	$unique_attachments = [];
 
-  foreach ($attachments as $attachment) {
-    if (!isset($uniqueAttachments[$attachment->guid])) {
-      $uniqueAttachments[$attachment->guid] = $attachment;
-    }
-  }
+	foreach ( $attachments as $attachment ) {
+		if ( ! isset( $unique_attachments[ $attachment->guid ] ) ) {
+			$unique_attachments[ $attachment->guid ] = $attachment;
+		}
+	}
 
-  return array_values($uniqueAttachments);
+	return array_values( $unique_attachments );
 }
 
+/**
+ * Get all images
+ *
+ * @return mixed[] Returns array of images
+ */
 function filter_ai_get_images() {
-  $args = array(
-    'post_type' => 'attachment',
-    'post_mime_type' => filter_ai_mime_types(),
-    'posts_per_page' => -1,
-    'post_status' => 'inherit'    
-  );
+	$args = array(
+		'post_type'      => 'attachment',
+		'post_mime_type' => filter_ai_mime_types(),
+		'posts_per_page' => -1,
+		'post_status'    => 'inherit',
+	);
 
-  $attachments = get_posts($args);
+	$attachments = get_posts( $args );
 
-  return filter_ai_get_unique_attachments($attachments);
+	return filter_ai_get_unique_attachments( $attachments );
 }
 
+/**
+ * Get all images without alt text
+ *
+ * @return mixed[] Returns array of images without alt text
+ */
 function filter_ai_get_images_without_alt_text() {
-  $args = array(
-    'post_type' => 'attachment',
-    'post_mime_type' => filter_ai_mime_types(),
-    'posts_per_page' => -1,
-    'post_status' => 'inherit',
-    'meta_query' => array(
-      'relation' => 'OR',
-      array(
-        'key' => '_wp_attachment_image_alt',
-        'value' => '',
-        'compare' => 'NOT EXISTS',
-      ),
-      array(
-        'key' => '_wp_attachment_image_alt',
-        'value' => '',
-        'compare' => '='
-      )
-    )      
-  );
+	$args = array(
+		'post_type'      => 'attachment',
+		'post_mime_type' => filter_ai_mime_types(),
+		'posts_per_page' => -1,
+		'post_status'    => 'inherit',
+		'meta_query'     => array(
+			'relation' => 'OR',
+			array(
+				'key'     => '_wp_attachment_image_alt',
+				'value'   => '',
+				'compare' => 'NOT EXISTS',
+			),
+			array(
+				'key'     => '_wp_attachment_image_alt',
+				'value'   => '',
+				'compare' => '=',
+			),
+		),
+	);
 
-  $attachments = get_posts($args);
+	$attachments = get_posts( $args );
 
-  return filter_ai_get_unique_attachments($attachments);
+	return filter_ai_get_unique_attachments( $attachments );
 }
 
-function filter_ai_process_batch_image_alt_text($args) {
-  $imageId = $args['imageId'];
-  $userId = $args['userId'];
-  $currentUserId = get_current_user_id();
-  $metadata = wp_get_attachment_metadata($imageId);
-  $imageAltText = $metadata['_wp_attachment_image_alt'];
-  $imageFile = get_attached_file($imageId);
-  $imageMimeType = get_post_mime_type($imageId);
-  $imagePath = $imageFile;
+/**
+ * Generate image alt text
+ *
+ * @param array $args Object containing image_id and user_id
+ *
+ * @throws Exception If $image_page is empty
+ * @throws Exception If $image_mime_type is empty
+ * @throws Exception If no ai services are available
+ * @throws Exception If $text is empty
+ *
+ * @return void Returns early if the image already has alt text
+ */
+function filter_ai_process_batch_image_alt_text( $args ) {
+	$image_id        = $args['image_id'];
+	$user_id         = $args['user_id'];
+	$current_user_id = get_current_user_id();
+	$metadata        = wp_get_attachment_metadata( $image_id );
+	$image_alt_text  = $metadata['_wp_attachment_image_alt'];
+	$image_file      = get_attached_file( $image_id );
+	$image_mime_type = get_post_mime_type( $image_id );
+	$image_path      = $image_file;
 
-  if (isset($metadata['sizes']['medium'])) {
-    $imagePath = dirname($imageFile) . '/' . $metadata['sizes']['medium']['file'];
-  }
+	if ( isset( $metadata['sizes']['medium'] ) ) {
+		$image_path = dirname( $image_file ) . '/' . $metadata['sizes']['medium']['file'];
+	}
 
-  if (!empty($imageAltText)) {
-    return;
-  }
-  
-  if(empty($imagePath)) {
-    throw new Exception( esc_html__( 'Missing image path', 'filter-ai' ) );
-  }
+	if ( ! empty( $image_alt_text ) ) {
+		return;
+	}
 
-  if (empty($imageMimeType)) {
-    throw new Exception( esc_html__( 'Missing image mime type', 'filter-ai' ) );
-  }
+	if ( empty( $image_path ) ) {
+		throw new Exception( esc_html__( 'Missing image path', 'filter-ai' ) );
+	}
 
-  $required_capabilities = array(
-    'capabilities' => array(
-      AI_Capability::MULTIMODAL_INPUT,
-      AI_Capability::TEXT_GENERATION
-    )
-  );
+	if ( empty( $image_mime_type ) ) {
+		throw new Exception( esc_html__( 'Missing image mime type', 'filter-ai' ) );
+	}
 
-  try {
-    wp_set_current_user($userId);
+	$required_capabilities = array(
+		'capabilities' => array(
+			AI_Capability::MULTIMODAL_INPUT,
+			AI_Capability::TEXT_GENERATION,
+		),
+	);
 
-    if (ai_services()->has_available_services($required_capabilities) == false) {
-      throw new Exception( __( 'AI service not available', 'filter-ai' ) );
-    }
+	try {
+		wp_set_current_user( $user_id );
 
-    $service = ai_services()->get_available_service($required_capabilities);
+		if ( ai_services()->has_available_services( $required_capabilities ) === false ) {
+			throw new Exception( __( 'AI service not available', 'filter-ai' ) );
+		}
 
-    $parts = new Parts();
+		$service = ai_services()->get_available_service( $required_capabilities );
 
-    $prompt = __( 'Please generate a short description no more than 50 words for the following image that can be used as its alternative text. The description should be clear, succinct, and provide a sense of what the image portrays, ensuring that it is accessible to individuals using screen readers.', 'filter-ai' );
+		$parts = new Parts();
 
-    $settings = get_option('filter_ai_settings');
+		$prompt = __( 'Please generate a short description no more than 50 words for the following image that can be used as its alternative text. The description should be clear, succinct, and provide a sense of what the image portrays, ensuring that it is accessible to individuals using screen readers.', 'filter-ai' );
 
-    if (!empty($settings['image_alt_text_prompt'])) {
-      $prompt = $settings['image_alt_text_prompt'];
-    }
+		$settings = get_option( 'filter_ai_settings' );
 
-    $parts->add_text_part($prompt);
+		if ( ! empty( $settings['image_alt_text_prompt'] ) ) {
+			$prompt = $settings['image_alt_text_prompt'];
+		}
 
-    $imageData = file_get_contents($imagePath);
-    $imageBase64 = 'data:' . $imageMimeType . ';base64,' . base64_encode($imageData);
+		$parts->add_text_part( $prompt );
 
-    $parts->add_file_data_part($imageMimeType, $imageBase64);
+		// phpcs:ignore WordPress.WP.AlternativeFunctions.file_get_contents_file_get_contents
+		$image_data = file_get_contents( $image_path );
 
-    $content = new Content(Content_Role::USER, $parts);
+		// phpcs:ignore WordPress.PHP.DiscouragedPHPFunctions.obfuscation_base64_encode
+		$image_base_64 = 'data:' . $image_mime_type . ';base64,' . base64_encode( $image_data );
 
-    $candidates = $service->get_model(
-      array(
-        'feature' => 'filter-ai-image-alt-text',
-        $required_capabilities
-      )
-    )->generate_text($content);
+		$parts->add_file_data_part( $image_mime_type, $image_base_64 );
 
-    $text = Helpers::get_text_from_contents(
-      Helpers::get_candidate_contents($candidates)
-    );
+		$content = new Content( Content_Role::USER, $parts );
 
-    if (empty($text)) {
-      throw new Exception( esc_html__( 'Issue generating alt text', 'filter-ai' ) );
-    }
-    
-    update_post_meta($imageId, '_wp_attachment_image_alt', $text);
-  } finally {
-    wp_set_current_user($currentUserId);
-  }
+		$candidates = $service->get_model(
+			array(
+				'feature' => 'filter-ai-image-alt-text',
+				$required_capabilities,
+			)
+		)->generate_text( $content );
+
+		$text = Helpers::get_text_from_contents(
+			Helpers::get_candidate_contents( $candidates )
+		);
+
+		if ( empty( $text ) ) {
+			throw new Exception( esc_html__( 'Issue generating alt text', 'filter-ai' ) );
+		}
+
+		update_post_meta( $image_id, '_wp_attachment_image_alt', $text );
+	} finally {
+		wp_set_current_user( $current_user_id );
+	}
 }
 
-add_action('filter_ai_batch_image_alt_text', 'filter_ai_process_batch_image_alt_text');
+add_action( 'filter_ai_batch_image_alt_text', 'filter_ai_process_batch_image_alt_text' );
 
+/**
+ * Reset group_id for scheduled actions to help us track the current actions
+ */
 function filter_ai_reset_batch_image_alt_text() {
-  $actions = as_get_scheduled_actions(
-    array(
-      'hook' => 'filter_ai_batch_image_alt_text',
-      'group' => 'filter-ai-current',
-      'per_page' => 1
-    ),
-    'ids'
-  );
+	$actions = as_get_scheduled_actions(
+		array(
+			'hook'     => 'filter_ai_batch_image_alt_text',
+			'group'    => 'filter-ai-current',
+			'per_page' => 1,
+		),
+		'ids'
+	);
 
-  $inProgressActions = as_get_scheduled_actions(
-    array(
-      'hook' => 'filter_ai_batch_image_alt_text',
-      'status' => array(
-        ActionScheduler_Store::STATUS_PENDING, 
-        ActionScheduler_Store::STATUS_RUNNING
-      ),
-      'group' => 'filter-ai-current',
-      'per_page' => 1
-    ),
-    'ids'
-  );
+	$in_progress_actions = as_get_scheduled_actions(
+		array(
+			'hook'     => 'filter_ai_batch_image_alt_text',
+			'status'   => array(
+				ActionScheduler_Store::STATUS_PENDING,
+				ActionScheduler_Store::STATUS_RUNNING,
+			),
+			'group'    => 'filter-ai-current',
+			'per_page' => 1,
+		),
+		'ids'
+	);
 
-  if (!empty($actions) && empty($inProgressActions)) {
-    global $wpdb;
+	if ( ! empty( $actions ) && empty( $in_progress_actions ) ) {
+		global $wpdb;
 
-    $wpdb->query(
-      $wpdb->prepare(
-        "UPDATE {$wpdb->prefix}actionscheduler_actions
+		$wpdb->query(
+			$wpdb->prepare(
+				"UPDATE {$wpdb->prefix}actionscheduler_actions
         SET `group_id` = (
           SELECT group_id FROM {$wpdb->prefix}actionscheduler_groups WHERE slug = ''
         )
         WHERE `hook` = %s AND `group_id` = (
           SELECT group_id FROM {$wpdb->prefix}actionscheduler_groups WHERE slug = %s
         )",
-        'filter_ai_batch_image_alt_text',
-        'filter-ai-current'
-      )
-    );
-  }
+				'filter_ai_batch_image_alt_text',
+				'filter-ai-current'
+			)
+		);
+	}
 }
 
+/**
+ * API handler to trigger batch generation of image alt text
+ */
 function filter_ai_api_batch_image_alt_text() {
-  check_ajax_referer('filter_ai_api', 'nonce');
+	check_ajax_referer( 'filter_ai_api', 'nonce' );
 
-  filter_ai_reset_batch_image_alt_text();
+	filter_ai_reset_batch_image_alt_text();
 
-  $images = filter_ai_get_images_without_alt_text();
-     
-  if (!empty($images)) {
-    foreach($images as $image) {
-      // call action through a scheduled action
-      as_enqueue_async_action(
-        'filter_ai_batch_image_alt_text',
-        array(
-          array(
-            'imageId' => $image->ID, 
-            'userId' => get_current_user_id()
-          )
-        ),
-        'filter-ai-current'
-      );
+	$images = filter_ai_get_images_without_alt_text();
 
-      // call action instantly (user needs to stay on the page)
-      // do_action(
-      //   'filter_ai_batch_image_alt_text', 
-      //   array(
-      //     'imageId' => $image->ID, 
-      //     'userId' => get_current_user_id()
-      //   )
-      // );
-    }
-  }
+	if ( ! empty( $images ) ) {
+		foreach ( $images as $image ) {
+			// call action through a scheduled action
+			as_enqueue_async_action(
+				'filter_ai_batch_image_alt_text',
+				array(
+					array(
+						'image_id' => $image->ID,
+						'user_id'  => get_current_user_id(),
+					),
+				),
+				'filter-ai-current'
+			);
 
-  wp_send_json_success();
+			// call action instantly (user needs to stay on the page)
+			// do_action(
+			// 'filter_ai_batch_image_alt_text',
+			// array(
+			// 'image_id' => $image->ID,
+			// 'user_id' => get_current_user_id()
+			// )
+			// );
+		}
+	}
+
+	wp_send_json_success();
 }
 
-add_action('wp_ajax_filter_ai_api_batch_image_alt_text', 'filter_ai_api_batch_image_alt_text');
+add_action( 'wp_ajax_filter_ai_api_batch_image_alt_text', 'filter_ai_api_batch_image_alt_text' );
 
-function filter_ai_get_last_log_for_action_id($actionId) {
-  if (!isset($actionId)) {
-    return null;
-  }
+/**
+ * Get the last log message for a scheduled action
+ *
+ * @param int $action_id Scheduled action ID
+ *
+ * @return null|string Return log message
+ */
+function filter_ai_get_last_log_for_action_id( $action_id ) {
+	if ( ! isset( $action_id ) ) {
+		return null;
+	}
 
-  global $wpdb;
+	global $wpdb;
 
-  $log = null;
+	$log = null;
 
-  try {
-    $log = $wpdb->get_row(
-      $wpdb->prepare(
-        "SELECT `message` FROM {$wpdb->prefix}actionscheduler_logs
+	try {
+		$log = $wpdb->get_row(
+			$wpdb->prepare(
+				"SELECT `message` FROM {$wpdb->prefix}actionscheduler_logs
         WHERE action_id = %s
         ORDER BY log_date_gmt DESC
         LIMIT 1",
-        $actionId  
-      )
-    );
-  } catch(Exception $e) {}
+				$action_id
+			)
+		);
+	} catch ( Exception $e ) {
+		$log = null;
+	}
 
-  return $log;
+	return $log->message;
 }
 
+/**
+ * API handler to get the image counts
+ */
 function filter_ai_api_get_image_count() {
-  check_ajax_referer('filter_ai_api', 'nonce');
-  
-  $actions = as_get_scheduled_actions(
-    array(
-      'hook' => 'filter_ai_batch_image_alt_text',
-      'group' => 'filter-ai-current',
-      'per_page' => -1
-    ),
-    'ids'
-  );
+	check_ajax_referer( 'filter_ai_api', 'nonce' );
 
-  $pendingActions = as_get_scheduled_actions(
-    array(
-      'hook' => 'filter_ai_batch_image_alt_text',
-      'status' => ActionScheduler_Store::STATUS_PENDING,
-      'group' => 'filter-ai-current',
-      'per_page' => -1
-    ),
-    'ids'
-  );
+	$actions = as_get_scheduled_actions(
+		array(
+			'hook'     => 'filter_ai_batch_image_alt_text',
+			'group'    => 'filter-ai-current',
+			'per_page' => -1,
+		),
+		'ids'
+	);
 
-  $runningActions = as_get_scheduled_actions(
-    array(
-      'hook' => 'filter_ai_batch_image_alt_text',
-      'status' => ActionScheduler_Store::STATUS_RUNNING,
-      'group' => 'filter-ai-current',
-      'per_page' => -1
-    ),
-    'ids'
-  );
+	$pending_actions = as_get_scheduled_actions(
+		array(
+			'hook'     => 'filter_ai_batch_image_alt_text',
+			'status'   => ActionScheduler_Store::STATUS_PENDING,
+			'group'    => 'filter-ai-current',
+			'per_page' => -1,
+		),
+		'ids'
+	);
 
-  $completeActions = as_get_scheduled_actions(
-    array(
-      'hook' => 'filter_ai_batch_image_alt_text',
-      'status' => ActionScheduler_Store::STATUS_COMPLETE,
-      'group' => 'filter-ai-current',
-      'per_page' => -1
-    ),
-    'ids'
-  );
+	$running_actions = as_get_scheduled_actions(
+		array(
+			'hook'     => 'filter_ai_batch_image_alt_text',
+			'status'   => ActionScheduler_Store::STATUS_RUNNING,
+			'group'    => 'filter-ai-current',
+			'per_page' => -1,
+		),
+		'ids'
+	);
 
-  $failedActionsRaw = as_get_scheduled_actions(
-    array(
-      'hook' => 'filter_ai_batch_image_alt_text',
-      'status' => ActionScheduler_Store::STATUS_FAILED,
-      'group' => 'filter-ai-current',
-      'per_page' => -1
-    ),
-    'OBJECT'
-  );
+	$complete_actions = as_get_scheduled_actions(
+		array(
+			'hook'     => 'filter_ai_batch_image_alt_text',
+			'status'   => ActionScheduler_Store::STATUS_COMPLETE,
+			'group'    => 'filter-ai-current',
+			'per_page' => -1,
+		),
+		'ids'
+	);
 
-  $failedActions = array();
+	$failed_actions_raw = as_get_scheduled_actions(
+		array(
+			'hook'     => 'filter_ai_batch_image_alt_text',
+			'status'   => ActionScheduler_Store::STATUS_FAILED,
+			'group'    => 'filter-ai-current',
+			'per_page' => -1,
+		),
+		'OBJECT'
+	);
 
-  if (!empty($failedActionsRaw)) {
-    foreach($failedActionsRaw as $actionId => $action) {
-      $log = filter_ai_get_last_log_for_action_id($actionId);
+	$failed_actions = array();
 
-      $failedActions[] = array(
-        'image_id' => $action->get_args()[0]['imageId'],
-        'message' => $log->message
-      );
-    }
-  }
+	if ( ! empty( $failed_actions_raw ) ) {
+		foreach ( $failed_actions_raw as $action_id => $action ) {
+			$log = filter_ai_get_last_log_for_action_id( $action_id );
 
-  wp_send_json_success(
-    array(
-      'images_count' => count(filter_ai_get_images()),
-      'images_without_alt_text_count' => count(filter_ai_get_images_without_alt_text()),
-      'actions_count' => count($actions),
-      'pending_actions_count' => count($pendingActions),
-      'running_actions_count' => count($runningActions),
-      'complete_actions_count' => count($completeActions),
-      'failed_actions_count' => count($failedActions),
-      'failed_actions' => $failedActions
-    )
-  );
+			$failed_actions[] = array(
+				'image_id' => $action->get_args()[0]['image_id'],
+				'message'  => $log,
+			);
+		}
+	}
+
+	wp_send_json_success(
+		array(
+			'images_count'                  => count( filter_ai_get_images() ),
+			'images_without_alt_text_count' => count( filter_ai_get_images_without_alt_text() ),
+			'actions_count'                 => count( $actions ),
+			'pending_actions_count'         => count( $pending_actions ),
+			'running_actions_count'         => count( $running_actions ),
+			'complete_actions_count'        => count( $complete_actions ),
+			'failed_actions_count'          => count( $failed_actions ),
+			'failed_actions'                => $failed_actions,
+		)
+	);
 }
 
-add_action('wp_ajax_filter_ai_api_get_image_count', 'filter_ai_api_get_image_count');
+add_action( 'wp_ajax_filter_ai_api_get_image_count', 'filter_ai_api_get_image_count' );
 
+/**
+ * API handler to cancel pending scheduled actions
+ */
 function filter_ai_api_cancel_batch_image_alt_text() {
-  check_ajax_referer('filter_ai_api', 'nonce');
+	check_ajax_referer( 'filter_ai_api', 'nonce' );
 
-  as_unschedule_all_actions('filter_ai_batch_image_alt_text');
+	as_unschedule_all_actions( 'filter_ai_batch_image_alt_text' );
 
-  wp_send_json_success();
+	wp_send_json_success();
 }
 
-add_action('wp_ajax_filter_ai_api_cancel_batch_image_alt_text', 'filter_ai_api_cancel_batch_image_alt_text');
-
+add_action( 'wp_ajax_filter_ai_api_cancel_batch_image_alt_text', 'filter_ai_api_cancel_batch_image_alt_text' );
