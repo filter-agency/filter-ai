1ccc66a9237239650c189a4e04f8529fb2528635
Robert Meacher <robert@filteragency.com>
2025-07-03 10:33:52 +0100

    Change logic for dynamically replacing alt text

--- includes/dynamicReplaceAltText.php
+++ includes/dynamicReplaceAltText.php
@@ -10,41 +10,41 @@ require_once 'settings.php';
  * Render block filter to replace empty alt text
  *
  * @param string $block_content The block content
- * @param array  $block The full block including name and attributes
  *
  * @return string $block_content Block content
  */
-function filter_ai_render_block( $block_content, $block ) {
+function filter_ai_render_block( $block_content ) {
 	$settings = filter_ai_get_settings();
 
 	if ( ! $settings['dynamic_add_alt_text_enabled'] ) {
 		return $block_content;
 	}
 
-	$alt_attr = $block['attrs']['alt'];
-	$image_id = $block['attrs']['id'];
+	return preg_replace_callback(
+		'/<img[^>]+class="[^"]*wp-image-\d+[^"]*"[^>]*>/i',
+		function ( $matches ) {
+			if ( ! str_contains( $matches[0], 'alt=""' ) ) {
+				return $matches[0];
+			}
 
-	if ( ! empty( $block['attrs']['mediaAlt'] ) ) {
-		$alt_attr = $block['attrs']['mediaAlt'];
-	}
+			preg_match( '/wp-image-(\d+)/', $matches[0], $id_matches );
 
-	if ( ! empty( $block['attrs']['mediaId'] ) ) {
-		$image_id = $block['attrs']['mediaId'];
-	}
+			if ( empty( $id_matches[1] ) ) {
+				return $matches[0];
+			}
 
-	if ( ! empty( $alt_attr ) || empty( $image_id ) ) {
-		return $block_content;
-	}
+			$alt = get_post_meta( $id_matches[1], '_wp_attachment_image_alt', true );
 
-	$alt = get_post_meta( $image_id, '_wp_attachment_image_alt', true );
+			if ( empty( $alt ) ) {
+				return $matches[0];
+			}
 
-	if ( empty( $alt ) ) {
-		return $block_content;
-	}
-
-	return preg_replace( '/alt=\\"\\"/', 'alt="' . $alt . '"', $block_content );
+			return str_replace( 'alt=""', 'alt="' . htmlspecialchars( $alt ) . '"', $matches[0] );
+		},
+		$block_content
+	);
 }
 
-add_filter( 'render_block_core/image', 'filter_ai_render_block', 10, 2 );
-add_filter( 'render_block_core/cover', 'filter_ai_render_block', 10, 2 );
-add_filter( 'render_block_core/media-text', 'filter_ai_render_block', 10, 2 );
+add_filter( 'render_block_core/image', 'filter_ai_render_block', 10, 1 );
+add_filter( 'render_block_core/cover', 'filter_ai_render_block', 10, 1 );
+add_filter( 'render_block_core/media-text', 'filter_ai_render_block', 10, 1 );
