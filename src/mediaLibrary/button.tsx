d4239440543c04adea4ce55dc7d18f2b47559544
Robert Meacher <robert@filteragency.com>
2025-09-09 11:12:29 +0100

    Fix alt text generation for the classic editor
    Make alt text generation available when using the classic editor.

--- src/mediaLibrary/button.tsx
+++ src/mediaLibrary/button.tsx
@@ -1,7 +1,7 @@
 import { DropdownMenu } from '@/components/dropdownMenu';
 import { useSettings } from '@/settings';
 import { ai, hideLoadingMessage, showLoadingMessage, showNotice } from '@/utils';
-import { useEffect, useMemo, useState } from '@wordpress/element';
+import { useMemo } from '@wordpress/element';
 import { __ } from '@wordpress/i18n';
 import _ from 'underscore';
 import { usePrompts } from '@/utils/ai/prompts/usePrompts';
@@ -11,79 +11,9 @@ const maxPixelSize = 2000;
 const Events = _.extend({}, window?.Backbone?.Events);
 
 (() => {
-  const AttachmentDetails =
-    window.wp?.media?.view?.Attachment?.Details?.TwoColumn || window.wp?.media?.view?.Attachment?.Details;
-
-  if (!AttachmentDetails) return;
-
-  const OverrideAttachmentDetails = AttachmentDetails.extend({
-    render: function () {
-      AttachmentDetails.prototype.render.apply(this, arguments);
-
-      this.listenTo(Events, 'filter-ai:generateAltText', this.generateAltText);
-
-      Events.trigger('filter-ai:generateAltTextEnabled', true);
-    },
-    remove: function () {
-      AttachmentDetails.prototype.remove.apply(this);
-
-      this.stopListening(Events, 'filter-ai:generateAltText', this.generateAltText);
-
-      Events.trigger('filter-ai:generateAltTextEnabled', false);
-    },
-    async generateAltText(customPrompt?: string) {
-      showLoadingMessage(__('Alt Text', 'filter-ai'));
-
-      try {
-        if (!this.model.get('sizes')?.medium?.url) {
-          if (this.model.get('width') > maxPixelSize || this.model.get('height') > maxPixelSize) {
-            throw new Error(__('Please choose a smaller image.', 'filter-ai'));
-          }
-        }
-
-        const url = this.model.get('sizes')?.medium?.url || this.model.get('url');
-
-        const altText = await ai.getAltTextFromUrl(url, this.model.get('alt'), customPrompt);
-
-        if (!altText) {
-          throw new Error(__('Sorry, there has been an issue while generating your alt text.', 'filter-ai'));
-        }
-
-        this.model.set('alt', altText);
-
-        this.model.save();
-
-        this.render();
-
-        showNotice({ message: __('Alt text has been updated.', 'filter-ai') });
-      } catch (error) {
-        console.error(error);
-
-        // @ts-expect-error Property 'message' does not exist on type '{}'
-        showNotice({ message: error?.message || error, type: 'error' });
-      } finally {
-        hideLoadingMessage();
-      }
-    },
-  });
-
-  if (window.wp?.media.view.Attachment.Details.TwoColumn) {
-    window.wp.media.view.Attachment.Details.TwoColumn = OverrideAttachmentDetails;
-  } else if (window.wp?.media.view.Attachment.Details) {
-    window.wp.media.view.Attachment.Details = OverrideAttachmentDetails;
-  }
-})();
-
-(() => {
-  const Modal = window.wp?.media?.view?.Modal;
-
-  if (!Modal) return;
-
   const { createRoot } = window.wp?.element || {};
 
   const ModalButton = () => {
-    const [generateAltTextEnabled, setGenerateAltTextEnabled] = useState(false);
-
     const { settings } = useSettings();
 
     const prompt = usePrompts('image_alt_text_prompt');
@@ -97,46 +27,97 @@ const Events = _.extend({}, window?.Backbone?.Events);
           onClick: () => {
             Events.trigger('filter-ai:generateAltText', prompt);
           },
-          isDisabled: !generateAltTextEnabled,
         });
       }
 
       return options;
-    }, [settings, generateAltTextEnabled]);
-
-    useEffect(() => {
-      Events.listenTo(Events, 'filter-ai:generateAltTextEnabled', setGenerateAltTextEnabled);
-
-      if (document.documentElement.querySelector('.attachment-details .alt-text')) {
-        setGenerateAltTextEnabled(true);
-      }
-
-      return () => {
-        Events.stopListening(Events, 'filter-ai:generateAltTextEnabled', setGenerateAltTextEnabled);
-      };
-    }, []);
+    }, [settings]);
 
     return <DropdownMenu controls={controls} />;
   };
 
-  const OverrideModal = Modal.extend({
-    render: function () {
-      Modal.prototype.render.apply(this, arguments);
+  const extend = (AttachmentDetails: any) =>
+    AttachmentDetails.extend({
+      render: function () {
+        AttachmentDetails.prototype.render.apply(this, arguments);
 
-      this.$el.find('.media-frame-title').addClass('filter-ai-media-frame-title');
+        this.listenTo(Events, 'filter-ai:generateAltText', this.generateAltText);
 
-      const container = document.createElement('div');
-      container.id = 'filter-ai-media-modal-container';
+        const modal = this.controller?.content?.view || this.modal;
 
-      const root = createRoot?.(container);
+        if (modal && !document.getElementById('filter-ai-media-modal-container')) {
+          modal.$el.find('.media-frame-title').addClass('filter-ai-media-frame-title');
 
-      this.$el.find('.edit-media-header').addClass('filter-ai-edit-media-header');
+          const container = document.createElement('div');
+          container.id = 'filter-ai-media-modal-container';
 
-      this.$el.find('.media-modal-close').before(container);
+          const root = createRoot?.(container);
 
-      root.render(<ModalButton />);
-    },
-  });
+          modal.$el.find('.edit-media-header').addClass('filter-ai-edit-media-header');
 
-  window.wp.media.view.Modal = OverrideModal;
+          const button = modal.$el.find('.media-modal-close').before(container);
+
+          if (!button.length) {
+            modal.$el.find('.media-frame-title').append(container);
+          }
+
+          root.render(<ModalButton />);
+        }
+
+        return this;
+      },
+      remove: function () {
+        AttachmentDetails.prototype.remove.apply(this);
+
+        this.stopListening(Events, 'filter-ai:generateAltText', this.generateAltText);
+      },
+      async generateAltText(customPrompt?: string) {
+        showLoadingMessage(__('Alt Text', 'filter-ai'));
+
+        try {
+          if (!this.model.get('sizes')?.medium?.url) {
+            if (this.model.get('width') > maxPixelSize || this.model.get('height') > maxPixelSize) {
+              throw new Error(__('Please choose a smaller image.', 'filter-ai'));
+            }
+          }
+
+          const url = this.model.get('sizes')?.medium?.url || this.model.get('url');
+
+          const altText = await ai.getAltTextFromUrl(url, this.model.get('alt'), customPrompt);
+
+          if (!altText) {
+            throw new Error(__('Sorry, there has been an issue while generating your alt text.', 'filter-ai'));
+          }
+
+          this.model.set('alt', altText);
+
+          try {
+            this.model.save();
+          } catch (e) {}
+
+          this.render();
+
+          showNotice({ message: __('Alt text has been updated.', 'filter-ai') });
+        } catch (error) {
+          console.error(error);
+
+          // @ts-expect-error Property 'message' does not exist on type '{}'
+          showNotice({ message: error?.message || error, type: 'error' });
+        } finally {
+          hideLoadingMessage();
+        }
+      },
+    });
+
+  if (window?.wp?.media?.view?.Attachment?.Details) {
+    window.wp.media.view.Attachment.Details = extend(window.wp.media.view.Attachment.Details);
+  }
+
+  if (window?.wp?.media?.view?.Attachment?.Details?.TwoColumn) {
+    window.wp.media.view.Attachment.Details.TwoColumn = extend(window.wp.media.view.Attachment.Details.TwoColumn);
+  }
+
+  if (window?.wp?.media?.view?.ImageDetails) {
+    window.wp.media.view.ImageDetails = extend(window.wp.media.view.ImageDetails);
+  }
 })();
