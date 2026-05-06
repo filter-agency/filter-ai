58a494188f6b0a6bd9f1f72f261aa6323a379324
Robert Meacher <robert@filteragency.com>
2026-05-06 10:00:00 +0100

    update composer

---
+++ vendor-prefixed/composer/installed.php
@@ -0,0 +1,66 @@
+<?php return array (
+  'root' => 
+  array (
+    'name' => 'filter-agency/filter-ai',
+    'pretty_version' => '1.6.0',
+    'version' => '1.6.0.0',
+    'reference' => NULL,
+    'type' => 'wordpress-plugin',
+    'install_path' => __DIR__ . '/../',
+    'aliases' => 
+    array (
+    ),
+    'dev' => true,
+  ),
+  'versions' => 
+  array (
+    'lucatume/di52' => 
+    array (
+      'pretty_version' => '4.0.1',
+      'version' => '4.0.1.0',
+      'reference' => '1552a86bf17db4cb098db368d2e4cbb508dcaec0',
+      'type' => 'library',
+      'install_path' => __DIR__ . '/../lucatume/di52',
+      'aliases' => 
+      array (
+      ),
+      'dev_requirement' => false,
+    ),
+    'psr/container' => 
+    array (
+      'pretty_version' => '1.1.2',
+      'version' => '1.1.2.0',
+      'reference' => '513e0666f7216c7459170d56df27dfcefe1689ea',
+      'type' => 'library',
+      'install_path' => __DIR__ . '/../psr/container',
+      'aliases' => 
+      array (
+      ),
+      'dev_requirement' => false,
+    ),
+    'stellarwp/container-contract' => 
+    array (
+      'pretty_version' => '1.1.1',
+      'version' => '1.1.1.0',
+      'reference' => 'b2c42c76681db314e4edbb2af0a312b6c06b495e',
+      'type' => 'library',
+      'install_path' => __DIR__ . '/../stellarwp/container-contract',
+      'aliases' => 
+      array (
+      ),
+      'dev_requirement' => false,
+    ),
+    'stellarwp/telemetry' => 
+    array (
+      'pretty_version' => '2.4.1',
+      'version' => '2.4.1.0',
+      'reference' => 'c1b3a43359475460a17589dd0442310a81f51b24',
+      'type' => 'library',
+      'install_path' => __DIR__ . '/../stellarwp/telemetry',
+      'aliases' => 
+      array (
+      ),
+      'dev_requirement' => false,
+    ),
+  ),
+);
---
+++ vendor-prefixed/lucatume/di52/.phpstorm.meta.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Filter_AI\Vendor\PHPSTORM_META {
+    $STATIC_METHOD_TYPES = [
+        \tad_DI52_Container::make( '' ) => [
+            "" == "@",
+        ],
+        \Filter_AI\Vendor\lucatume\DI52\Container::make( '' ) => [
+            "" == "@",
+        ],
+        \Filter_AI\Vendor\lucatume\DI52\Container::get( '' ) => [
+            "" == "@",
+        ],
+        \Filter_AI\Vendor\lucatume\DI52\App::make( '' ) => [
+            "" == "@",
+        ],
+        \Filter_AI\Vendor\lucatume\DI52\App::get( '' ) => [
+            "" == "@",
+        ],
+    ];
+}
